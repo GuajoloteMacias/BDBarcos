e8fcad924ebfafd7d3c46fe931fdec4a450e0a1f
GuajoloteMacias <61269005+GuajoloteMacias@users.noreply.github.com>
2020-02-20 01:12:08 -0700

    Add files via upload

---
+++ CRUD_Nautico_133401/src/crud_nautico_133401/ConnectionFactory.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crud_nautico_133401;
+
+/**
+ *
+ * @author Pollo
+ */
+public class ConnectionFactory {
+    
+}
